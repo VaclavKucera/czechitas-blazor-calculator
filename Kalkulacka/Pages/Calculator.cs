b16d3d5f94f07426d80835ec4d39cabea52614cc
Václav Kučera <vaclav.kucera@mews.com>
2024-04-02 18:13:15 +0200

    prvni verze funkcionality

---
+++ Kalkulacka/Pages/Calculator.cs
@@ -0,0 +1,27 @@
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Cannot divide by zero.");
+        }
+
+        return a / b;
+    }
+}
